a51f34d41a70d3b3b73827e09d4729abb0c24d58
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2022-07-31 20:51:09 +0600

    final

---
+++ Apps.js
@@ -0,0 +1,10 @@
+
+function subjectCodeData(id){
+    // console.log("Click Marce")
+    document.getElementById(id).style.display = 'block';
+}
+
+function RegistrationData(id){
+    document.getElementById(id).style.display='none';
+    document.getElementById("registrationData").style.display='block';
+}
